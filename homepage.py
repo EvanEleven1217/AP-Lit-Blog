959852e4c5bf2ca9abc642f1fb04177c3827528f
evan1217lee@gmail.com <evan1217lee@gmail.com>
2022-09-25 23:02:29 -0700

    9/25/22 11:02

--- homepage.py
+++ homepage.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import base64
 
 st.set_page_config(
     page_title="Evan's Blog"
@@ -8,3 +9,20 @@ st.title("Evan's Blog")
 st.sidebar.success("Select a page")
 
 
+
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('wallpaper.png') 
--- pages/about.py
+++ pages/about.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 
-st.write()
+st.write("About me")
