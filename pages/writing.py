3a7a5c4c4d4de539821096dd608a04da6e773b5c
evan1217lee@gmail.com <evan1217lee@gmail.com>
2022-09-26 20:24:30 -0700

    9/26/22 8:24pm

--- pages/writing.py
+++ pages/writing.py
@@ -1,5 +1,22 @@
 import streamlit as st
+import base64
 
 st.title("Writing")
 
 st.write("Nothing here as of now! ")
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('mountainBackground.jpg') 
